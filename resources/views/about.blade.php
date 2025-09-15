205dcb1e6375e7638cb3804e983d9b91228b4da0
fedebacci <federicobacci1999@gmail.com>
2025-09-15 10:54:52 +0200

    utilizzo informazioni in una view e collegamento di una seconda view

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,15 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel primi passi</title>
+    <title>About</title>
 </head>
 <body>
-    <h1>Hello World!</h1>
-
-    <p>
-        {{ $info_test }}
-    </p>
-
-    <a href="/about" target="_blank">About</a>
+    <h1>About us</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+
+    $info_test = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+    // return view('home', ['info_test' => $info_test]);
+    return view('home', compact('info_test'));
+});
+
+Route::get('/about', function () {
+    return view('about');
 });
